b6e6ce96c19239470a7c2e1f43fcae61bc07ca12
Omeiza <omeizaalabi@gmail.com>
2023-04-27 04:34:18 -0700

    Updae

--- _printf.c
+++ _printf.c
@@ -1,72 +1,90 @@
 #include "main.h"
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-#include <unistd.h>
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    int count = 0;
-    char buffer[1024];
-    int bufferIndex = 0;
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            if (*format == 'c') {
-                int c = va_arg(args, int);
-                buffer[bufferIndex++] = (char)c;
-            } else if (*format == 's') {
-                char *s = va_arg(args, char *);
-                int len = strlen(s);
-                memcpy(&buffer[bufferIndex], s, len);
-                bufferIndex += len;
-            } else if (*format == 'd' || *format == 'i') {
-                int d = va_arg(args, int);
-                bufferIndex += snprintf(&buffer[bufferIndex], sizeof(buffer) - bufferIndex, "%d", d);
-            } else if (*format == 'b') {
-                unsigned int b = va_arg(args, unsigned int);
-                char binary[32];
-                int i = 0;
-                while (b > 0) {
-                    binary[i++] = (b % 2) + '0';
-                    b /= 2;
-                }
-                binary[i] = '\0';
-                strrev(binary);
-                int len = strlen(binary);
-                memcpy(&buffer[bufferIndex], binary, len);
-                bufferIndex += len;
-            } else if (*format == 'u') {
-                unsigned int u = va_arg(args, unsigned int);
-                bufferIndex += snprintf(&buffer[bufferIndex], sizeof(buffer) - bufferIndex, "%u", u);
-            } else if (*format == 'o') {
-                unsigned int o = va_arg(args, unsigned int);
-                bufferIndex += snprintf(&buffer[bufferIndex], sizeof(buffer) - bufferIndex, "%o", o);
-            } else if (*format == 'x') {
-                unsigned int x = va_arg(args, unsigned int);
-                bufferIndex += snprintf(&buffer[bufferIndex], sizeof(buffer) - bufferIndex, "%x", x);
-            } else if (*format == 'X') {
-                unsigned int X = va_arg(args, unsigned int);
-                bufferIndex += snprintf(&buffer[bufferIndex], sizeof(buffer) - bufferIndex, "%X", X);
-            } else if (*format == '%') {
-                buffer[bufferIndex++] = '%';
-            }
-        } else {
-            buffer[bufferIndex++] = *format;
-        }
-        format++;
-        if (bufferIndex >= sizeof(buffer) - 1) {
-            write(STDOUT_FILENO, buffer, bufferIndex);
-            count += bufferIndex;
-            bufferIndex = 0;
-        }
-    }
-    if (bufferIndex > 0) {
-        write(STDOUT_FILENO, buffer, bufferIndex);
-        count += bufferIndex;
-    }
-    va_end(args);
-    return count;
+/**
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+/**
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
+ *
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	buffer_t *output;
+	va_list args;
+	int ret;
+
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	ret = run_printf(format, args, output);
+
+	return (ret);
 }
